8010efeff8c6da1dbd3a5002f1cd0c6793085f08
Nicholas Moore <nicholasvmoore@gmail.com>
2015-09-30 19:48:13 +0000

    adding udev rule

--- utils.sh
+++ utils.sh
@@ -2,3 +2,9 @@
 
 sudo apt-get update
 sudo apt-get install tmux zsh tcpdump bridge-utils wicd-curses telnet isc-dhcp-server bind9
+
+sudo sysctl -w net.ipv4.ip_forward=1
+
+sudo iptables -t nat -A POSTROUTING -j MASQUERADE
+sudo sh -c "iptables-save > /etc/network/iptables.ipv4.nat"
+# Restore up iptables-restore < /etc/network/iptables.ipv4.nat
